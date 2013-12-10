baf2072d82eef3340023fc638cd8f0b78d9f3060
yong.li <liyong19861014@gmail.com>
2013-12-10 15:21:57 +0800

    add CacheClientFactory add Entry add EntryConstant

--- src/main/java/sirius/cache/CacheClientFactory.java
+++ src/main/java/sirius/cache/CacheClientFactory.java
@@ -1,5 +1,36 @@
 package sirius.cache;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+
+/**
+ * 
+ * @author michael
+ * @email liyong19861014@gmail.com
+ */
 public class CacheClientFactory {
+
+    private static Logger logger = Constant.logger;
+
+    private static Map<Entry, CacheClient> entryCacheClientMap = new HashMap<Entry, CacheClient>();
+
+    public static CacheClient getCacheClient(final Entry entry) {
+        if (entry == null) {
+            logger.error("CacheClientFactory.getCacheClient() arg entry is null!");
+            return null;
+        }
+        synchronized (CacheClientFactory.class) {
+            CacheClient client = entryCacheClientMap.get(entry);
+            if (client == null) {
+                client = new CacheClient(entry.getNamespace(), entry.getBusiness());
+                if (client != null) {
+                    entryCacheClientMap.put(entry, client);
+                }
+            }
+            return client;
+        }
+    }
 
 }
---
+++ src/main/java/sirius/cache/EntryConstant.java
@@ -0,0 +1,13 @@
+package sirius.cache;
+
+/**
+ * Entry常量表
+ * 
+ * @author michael
+ * @email liyong19861014@gmail.com
+ */
+public interface EntryConstant {
+    
+    
+
+}
